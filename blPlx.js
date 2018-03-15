b560090d6bd6445b3af1f3d5dde6b63306c0d72d
littleflute <13699175164@sina.cn>
2018-03-15 14:17:48 -0700

    Create blPlx.js

---
+++ blPlx.js
@@ -0,0 +1,4 @@
+var _g_ver_blPlx_4_Mp3Player = "v0.0.1";
+var plxUI = blo0.blMDiv(document.body, "id_mv_blMp3Player_plx" , "blPlx: " + _g_ver_blPlx_4_Mp3Player , 0,0,150,50, blColor[4]);  
+plxUI.style.position = "fixed"; 
+plxUI.style.float = "right";
