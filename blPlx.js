dd3c170349a0371dd33b538c09975e1c075d365d
littleflute <13699175164@sina.cn>
2018-03-15 14:52:04 -0700

    Update blPlx.js

--- blPlx.js
+++ blPlx.js
@@ -1,4 +1,4 @@
-var _g_ver_blPlx_4_Mp3Player = "v0.0.13";
+var _g_ver_blPlx_4_Mp3Player = "v0.0.14";
 var plxUI = blo0.blMDiv(document.body, "id_mv_blMp3Player_plx" , "blPlx: " + _g_ver_blPlx_4_Mp3Player , 0,0,150,50, blGrey[3]);  
 plxUI.style.position = "fixed"; 
 plxUI.style.width = "100%";
