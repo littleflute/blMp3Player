a6bf7194ae79863c8e1d2c49d21a8805a3ed1bf1
littleflute <13699175164@sina.cn>
2018-03-15 14:50:10 -0700

    Update blPlx.js

--- blPlx.js
+++ blPlx.js
@@ -1,5 +1,10 @@
-var _g_ver_blPlx_4_Mp3Player = "v0.0.4";
-var plxUI = blo0.blMDiv(document.body, "id_mv_blMp3Player_plx" , "blPlx: " + _g_ver_blPlx_4_Mp3Player , 0,0,150,50, blGrey[4]);  
+var _g_ver_blPlx_4_Mp3Player = "v0.0.12";
+var plxUI = blo0.blMDiv(document.body, "id_mv_blMp3Player_plx" , "blPlx: " + _g_ver_blPlx_4_Mp3Player , 0,0,150,50, blGrey[3]);  
 plxUI.style.position = "fixed"; 
 plxUI.style.width = "100%";
+plxUI.handle.style.display = "none";
+plxUI.main.style.display = "none";
 plxUI.d1 = blo0.blDiv(plxUI, plxUI.id + "d1", "d1",blGrey[2]);
+plxUI.d1.v = blo0.blMDiv(plxUI.d1, plxUI.d1.id + "v",, "blPlx.d1.v" , 50,50,150,50, blGrey[3]);  
+plxUI.d1.v.style.position = "relative";
+plxUI.d1.v.style.float = "right";
